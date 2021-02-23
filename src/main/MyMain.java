53e95c672c041506d804f3713647e5a30c86070b
Vaibhav Diwan <divaibhav@gmail.com>
2021-02-23 12:58:43 +0530

    changes in main

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -5,7 +5,7 @@ import infix.postfix.InfixToPostfix;
 public class MyMain {
     public static void main(String[] args) {
         InfixToPostfix convertor = new InfixToPostfix();
-        String infix = "5+6^2";//"A+(B*C-(D/E^F)*G)*H";
+        String infix = "5*6/3";//"A+(B*C-(D/E^F)*G)*H";
         String postfix = convertor.infixToPostfix(infix);
         System.out.println("postfix = " + postfix);
         double result = convertor.postfixEvaluation(postfix);
